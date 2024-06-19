40382df5b20c63ede104877a950812da8b244a60
Kuvrot <jamessanz@outlook.com>
2024-06-19 15:46:38 -0600

    UI implementation

---
+++ src/main/java/com/clinichub/Clinichub.java
@@ -0,0 +1,11 @@
+package com.clinichub;
+import com.clinichub.GUI.Home;
+
+public class Clinichub {
+
+    public static void main(String[] args) {
+        Home home  = new Home();
+        home.setVisible(true);
+        home.setLocationRelativeTo(null);
+    }
+}
